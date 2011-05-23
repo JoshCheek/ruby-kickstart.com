8c4b4313db45f23d61b1879c29f3652c75f886c6
Josh Cheek <josh.cheek@gmail.com>
2011-05-23 01:13:35 -0500

    add database connectivity

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "http://rubygems.org"
-gem 'sinatra' , '1.3.0.d'
-gem 'haml'    , '3.1.1'
+gem 'activerecord'  , '3.0.7'
+gem 'sinatra'       , '1.3.0.d'
+gem 'haml'          , '3.1.1'
+gem 'sqlite3-ruby'  , '1. 3.3'
--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
+# NEED TO ADD SQLITE3 TO GEMFILE BEFORE COMMITTING
+# BUT I LOST INTERNET CONNECTION AND CAN'T LOOK UP GEM NAME
+
+require 'yaml'
+require 'bundler/setup'
 require 'sinatra'
+require 'active_record'
+
+# activerecord
+case settings.environment  
+when 'production', :production
+  database_file = "/data/rubykickstartcom/shared/config/database.yml"
+when 'development', :development
+  database_file = File.dirname(__FILE__) + "/db/database.yml"
+end
+database_config = YAML.load( File.read database_file )[ settings.environment.to_s ]
+ActiveRecord::Base.establish_connection(database_config)
+
+
 
 get '/' do
-  haml 'home'
+  haml :home
 end
