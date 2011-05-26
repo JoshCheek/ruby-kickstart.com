dfc0b4bd1df92a51c167adb891f7ec42c96d375f
Josh Cheek <josh.cheek@gmail.com>
2011-05-26 17:50:24 -0500

    fix specs to run in test env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
-require File.expand_path(
-  File.join File.dirname(__FILE__) , '..' , 'environment'
-)
+# set proper env
+ENV['DB'] = ENV["RACK_ENV"] = ENV['MERB_ENV'] = ENV['RAILS_ENV'] = 'test'
 
+# load the application code
+require File.expand_path("#{File.dirname __FILE__}/../environment")
+
+# since we're doing this in memory, have to explicitly run migrations
+ActiveRecord::Migration.verbose = false
+ActiveRecord::Migrator.migrate("#{$root}/db/migrations", nil)
