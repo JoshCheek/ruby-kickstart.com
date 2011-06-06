3c0fc4ca4c73c246b7a61653a4d3988b677a5ec2
Josh Cheek <josh.cheek@gmail.com>
2011-06-06 09:13:33 -0500

    check that the table updates as expected

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe User do
   
+  before :each do
+    clean_db
+  end
+  
   subject do
     User.new :provider => 'some provider',
              :uid      => 'some uid',
@@ -11,10 +15,18 @@ describe User do
   it 'should not be a new record after saving' do
     subject.save
     should_not be_new_record
+    subject.id.should == 1
   end
   
   its(:provider) { should == 'some provider' }
   its(:uid)      { should == 'some uid'      }
   its(:name)     { should == 'Josh Cheek'    }
+  
+  specify '.count should be 1 after saving' do
+    User.count.should == 0
+    subject.save
+    User.count.should == 1
+  end
+  
   
 end
