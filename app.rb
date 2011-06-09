5272ceb54459268ae7e3496dcd33ce73ed1bab9f
Josh Cheek <josh.cheek@gmail.com>
2011-06-09 14:07:58 -0500

    take out the weird fix and see if logging in works again

--- app.rb
+++ app.rb
@@ -5,8 +5,6 @@ helpers do
   
   def current_user
     @current_user ||= User.find session[:user_id] if session[:user_id]
-  rescue ActiveRecord::RecordNotFound
-    @current_user = session[:user_id] = nil
   end
   
   def logged_in?
