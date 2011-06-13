48e9e6704f9e64585f9f680b3e54420d05cfde61
Josh Cheek <josh.cheek@gmail.com>
2011-06-13 12:39:07 -0500

    remove stupid restriction that I think is what is killing my fucking app

--- db/migrations/20110613160252_switch_yaml_serialization_to_marshal.rb
+++ db/migrations/20110613160252_switch_yaml_serialization_to_marshal.rb
@@ -1,7 +1,7 @@
 class SwitchYamlSerializationToMarshal < ActiveRecord::Migration
   def self.up
     change_column :quiz_multiple_choice_problems, :serialized_options, :binary
-    change_column :quiz_regexes, :content, :binary
+    change_column :quiz_regexes, :content, :binary, :limit => nil # because it used to be a string and will retain its limit otherwise
   end
 
   def self.down
