b88b77aab8725453d91d3b6e877e4079aae94f8b
Josh Cheek <josh.cheek@gmail.com>
2011-06-29 13:38:50 -0500

    when adding version column, give existing quizzes version of 1

--- db/migrations/20110629002932_add_quiz_versioning.rb
+++ db/migrations/20110629002932_add_quiz_versioning.rb
@@ -1,9 +1,14 @@
 class AddQuizVersioning < ActiveRecord::Migration
+  
+  Quiz = Class.new ActiveRecord::Base
+  
   def self.up
     add_column :quizzes, :version, :integer
+    Quiz.all.each { |quiz| quiz.version = 1; quiz.save }
   end
 
   def self.down
     remove_column :quizzes, :version
   end
+  
 end
